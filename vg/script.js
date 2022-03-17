d9f711e278ffa2a6bef7386b1a32f61ebffd7e2f
Jon Sousa <90006421+chiefsous@users.noreply.github.com>
2022-03-17 16:40:48 -0400

    Add files via upload

--- vg/script.js
+++ vg/script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
   mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3700, npcCollisionRadius: 100, actionDuration:600, 
   jumpHeight: 350, makeResponsive: true, typesOfNPC: 3, removeNPCAfterInteracted: false, numOfCollectables: 3, collectableRadius: 100, 
-  pointsPerCollectable: 1, removeCollectableAfterCollected: true});
+  pointsPerCollectable: 1, removeCollectableAfterCollected: true, playUntilScore: 10, postGameScreenImageURLs: ["./img/background.png"]});
 });
