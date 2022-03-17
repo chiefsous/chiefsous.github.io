778ac146e42ebac881790decdd48766293861ae4
Jon Sousa <90006421+chiefsous@users.noreply.github.com>
2022-03-17 15:02:30 -0400

    Add files via upload

---
+++ vg/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3700, npcCollisionRadius: 100, actionDuration:600, 
+  jumpHeight: 350, makeResponsive: true, typesOfNPC: 3, removeNPCAfterInteracted: false, numOfCollectables: 3, collectableRadius: 100, 
+  pointsPerCollectable: 1, removeCollectableAfterCollected: true});
+});
